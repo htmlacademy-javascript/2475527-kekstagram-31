d45de419df587c2607c0f3f2aefde74cfcc3d4a4
Keks <keks@htmlacademy.ru>
2024-03-14 17:58:36 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/data.js
@@ -0,0 +1,43 @@
+import {getRandomInteger, getUniqueNumber, getRandomArrayElement} from './get-random.js';
+
+const NAMES = ['Валентина', 'Иван', 'Анна', 'Максим', 'Юлия', 'Александр', 'Марина', 'Ксения'];
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const DESCRIPTIONS = [
+  'А вы рыбов продаете?',
+  'Нет, только показываю',
+  'Meow',
+  'Nice day!',
+  'Всем привет!',
+];
+const CREATE_POST = 25;
+
+const idPhoto = getUniqueNumber();
+const idComment = getUniqueNumber();
+const urlNumber = getUniqueNumber();
+
+const createComment = () => ({
+  id: idComment(),
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES),
+});
+
+const createPhotoDescription = () => ({
+  id: idPhoto(),
+  url: `photos/${urlNumber()}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(15, 200),
+  comments: Array.from({length: getRandomInteger(0, 30)}, createComment),
+});
+
+const photoDescription = () => Array.from({length: CREATE_POST}, createPhotoDescription);
+//photoDescription();
+
+export { photoDescription };
---
+++ 5/js/main.js
@@ -0,0 +1,4 @@
+import { photoDescription } from './data.js';
+import { similarPhoto, pictureContainer } from './render-photos.js';
+
+similarPhoto(photoDescription(pictureContainer));
