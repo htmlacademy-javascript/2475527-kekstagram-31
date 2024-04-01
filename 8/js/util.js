10c9e92793443c98a71cf849ca35a52224b7c232
Keks <keks@htmlacademy.ru>
2024-04-01 16:09:29 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/util.js
@@ -0,0 +1,23 @@
+//Функция-генератор для получения случайных идентификаторов из указанного диапазона
+function getRandomInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
+// Функция создания уникального числа
+const getUniqueNumber = () => {
+  let number = 1;
+  return function () {
+    return number++;
+  };
+};
+
+// Функция получения рандомного элемента массива
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {getRandomInteger, getUniqueNumber, getRandomArrayElement, isEscapeKey};
