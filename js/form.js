a705b2c777dea18c46364820763b6527375a3bad
Anna <aa.dolgova@yandex.ru>
2024-04-02 14:19:10 +0300

    Scale and Effects

--- js/form.js
+++ js/form.js
@@ -1,5 +1,7 @@
 import { isEscapeKey } from './util.js';
 import { validateForm, resetValidator } from './validation.js';
+import { resetScale } from './scale.js';
+import { resetEffects } from './effects.js';
 
 const body = document.body;
 const form = document.querySelector('.img-upload__form');
@@ -13,6 +15,10 @@ const isTextFieldFocused = () =>
   document.activeElement === fieldHashtags ||
   document.activeElement === fieldDescription;
 
+const onUploadModalCloseClick = () => {
+  closeModal();
+};
+
 function onDocumentKeydown(evt) {
   if (isEscapeKey(evt) && !isTextFieldFocused()) {
     evt.preventDefault();
@@ -23,6 +29,7 @@ function onDocumentKeydown(evt) {
 function openModal() {
   uploadOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
+  uploadModalClose.addEventListener('click', onUploadModalCloseClick);
   document.addEventListener('keydown', onDocumentKeydown);
 }
 
@@ -32,13 +39,14 @@ function closeModal() {
   fieldDescription.value = '';
 
   resetValidator();
+  resetScale();
+  resetEffects();
   uploadOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
 }
 
 uploadInput.addEventListener('change', openModal);
-uploadModalClose.addEventListener('click', closeModal);
 
 validateForm();
 
