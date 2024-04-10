33f6663ddc983ef1b74feca9411de77c6516b09e
Anna <aa.dolgova@yandex.ru>
2024-04-10 12:56:25 +0300

    Filters

--- js/render-comments.js
+++ js/render-comments.js
@@ -1,19 +1,62 @@
 //import { COMMENTS_SHOW } from './data.js';
 const COMMENTS_SHOW = 5;
 
+let currentCount = 0;
+let comments = [];
+
 const modalBigPhoto = document.querySelector('.big-picture');
 const socialComments = modalBigPhoto.querySelector('.social__comments');
 const socialComment = socialComments.querySelector('.social__comment');
 const socialCommentShowCount = modalBigPhoto.querySelector('.social__comment-shown-count');
-const socialCommentTotalCount = modalBigPhoto.querySelector('.social__comment-total-count');
+//const socialCommentTotalCount = modalBigPhoto.querySelector('.social__comment-total-count');
 const commentsLoader = modalBigPhoto.querySelector('.comments-loader');
 
-const renderComments = (comments, i = 0, callback) => {
-  const commentsLength = comments.length;
-  const visibleCommentsCount = COMMENTS_SHOW + (COMMENTS_SHOW * i);
-  const invisibleComments = comments.slice(0, visibleCommentsCount);
+socialComments.innerHTML = '';
+const createComment = ({avatar, name, message}) => {
+  const comment = socialComment.cloneNode(true);
+  const author = comment.querySelector('.social__picture');
+  author.src = avatar;
+  author.alt = name;
+  comment.querySelector('.social__text').textContent = message;
+  return comment;
+};
+const renderNextComments = () => {
+  const fragment = document.createDocumentFragment();
+  const renderedComments = comments.slice(currentCount, currentCount + COMMENTS_SHOW);
+  const renderedCommentsLength = renderedComments.length + currentCount;
+  renderedComments.forEach((comment) => {
+    const commentItem = createComment(comment);
+    fragment.append(commentItem);
+  });
+  socialComments.append(fragment);
 
-  commentsLoader.removeEventListener('click', callback);
+  socialCommentShowCount.textContent = renderedCommentsLength;
+  if (renderedCommentsLength >= comments.length) {
+    commentsLoader.classList.add('hidden');
+  }
+  currentCount += COMMENTS_SHOW;
+};
+const clearComments = () => {
+  currentCount = 0;
+  socialComments.innerHTML = '';
+  commentsLoader.classList.remove('hidden');
+  commentsLoader.removeEventListener('click', renderNextComments);
+};
+const renderComments = (photoComments) => {
+  comments = photoComments;
+  renderNextComments();
+
+  commentsLoader.addEventListener('click', renderNextComments);
+};
+
+export {clearComments, renderComments};
+/*
+//const renderComments = (comments, i = 0, callback) => {
+  //const commentsLength = comments.length;
+  //const visibleCommentsCount = COMMENTS_SHOW + (COMMENTS_SHOW * i);
+  //const invisibleComments = comments.slice(0, visibleCommentsCount);
+
+  //commentsLoader.removeEventListener('click', callback);
 
   socialComments.innerHTML = '';
 
@@ -48,3 +91,4 @@ const clearComments = () => {
 };
 
 export {clearComments, renderComments};
+*/
--- js/render-photos.js
+++ js/render-photos.js
@@ -1,19 +1,30 @@
-import { openModalBigPhoto } from './render-big-photos.js';
-
 const pictureContainer = document.querySelector('.pictures');
+const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
 
-const renderPhotos = (photos) => {
-  pictureContainer.addEventListener('click', (evt) => {
-    const targetPicture = evt.target.closest('.picture');
-    if(!targetPicture) {
-      return;
-    }
-    evt.preventDefault();
-    const targetPictureId = targetPicture.getAttribute('data-id');
-    const targetPhoto = photos.find((photo) => photo.id === +targetPictureId);
-
-    openModalBigPhoto(targetPhoto);
-  });
+const createPhotos = ({ url, description, likes, comments, id }) => {
+  const similarListPhoto = templatePicture.cloneNode(true);
+  similarListPhoto.href = url;
+  similarListPhoto.dataset.pictureId = id;
+  similarListPhoto.querySelector('.picture__img').src = url;
+  similarListPhoto.querySelector('.picture__img').alt = description;
+  similarListPhoto.querySelector('.picture__comments').textContent = comments.length;
+  similarListPhoto.querySelector('.picture__likes').textContent = likes;
+  return similarListPhoto;
 };
 
-export { renderPhotos };
+const renderPhotos = (pictures) => {
+  const similarListFragment = document.createDocumentFragment();
+
+  pictures.forEach((picture) => {
+    const similarListPhoto = createPhotos(picture);
+    similarListFragment.append(similarListPhoto);
+  });
+  pictureContainer.append(similarListFragment);
+};
+
+const removePhotos = () => {
+  document.querySelectorAll('.picture').forEach((picture) => picture.remove());
+};
+
+export { renderPhotos, removePhotos };
+
