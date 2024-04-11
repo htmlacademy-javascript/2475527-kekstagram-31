2dbfc80870751e467182051acc8168e0f20764af
Anna <aa.dolgova@yandex.ru>
2024-04-11 09:24:21 +0300

    Fix

--- js/render-comments.js
+++ js/render-comments.js
@@ -8,7 +8,6 @@ const modalBigPhoto = document.querySelector('.big-picture');
 const socialComments = modalBigPhoto.querySelector('.social__comments');
 const socialComment = socialComments.querySelector('.social__comment');
 const socialCommentShowCount = modalBigPhoto.querySelector('.social__comment-shown-count');
-//const socialCommentTotalCount = modalBigPhoto.querySelector('.social__comment-total-count');
 const commentsLoader = modalBigPhoto.querySelector('.comments-loader');
 
 socialComments.innerHTML = '';
@@ -50,45 +49,4 @@ const renderComments = (photoComments) => {
 };
 
 export {clearComments, renderComments};
-/*
-//const renderComments = (comments, i = 0, callback) => {
-  //const commentsLength = comments.length;
-  //const visibleCommentsCount = COMMENTS_SHOW + (COMMENTS_SHOW * i);
-  //const invisibleComments = comments.slice(0, visibleCommentsCount);
 
-  //commentsLoader.removeEventListener('click', callback);
-
-  socialComments.innerHTML = '';
-
-  for (const element of invisibleComments) {
-    const comment = socialComment.cloneNode(true);
-    comment.querySelector('.social__picture').src = element.avatar;
-    comment.querySelector('.social__picture').alt = element.name;
-    comment.querySelector('.social__text').textContent = element.message;
-
-    socialCommentShowCount.textContent = invisibleComments.length;
-
-    socialComments.append(comment);
-  }
-
-  socialCommentTotalCount.textContent = commentsLength;
-
-  if (commentsLength < COMMENTS_SHOW || commentsLength <= visibleCommentsCount) {
-    commentsLoader.classList.add('hidden');
-  } else {
-    commentsLoader.classList.remove('hidden');
-  }
-
-  const clickHandler = () => {
-    renderComments(comments, i + 1, clickHandler);
-  };
-
-  commentsLoader.addEventListener('click', clickHandler);
-};
-
-const clearComments = () => {
-  socialComment.forEach((element) => element.remove());
-};
-
-export {clearComments, renderComments};
-*/
